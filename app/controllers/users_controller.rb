9670a1871cb20c9ba0e76d3e4f57ea4514c45f70
Jason Fisher <jfisher@jfisher-15mbpr.ch.int>
2015-08-13 16:47:29 -0700

    added logout link and functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def self
+
+  end
+
+  def other
+  end
+
+  def all
+  end
+
+  def index
+    if :user_signed_in?
+      logger.warn "USER SIGNED IN"
+      redirect_to  '/users/signout'
+    else
+
+    end
+  end
+
+  def signout
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/self'
+
+  get 'users/other'
+
+  get 'users/all'
+
+  get 'users/signout'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to: 'home#index'
+  root to: 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
