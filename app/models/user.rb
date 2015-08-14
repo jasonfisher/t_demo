2c1b812c5dcfb9357beea3d28fc66e7c4f450483
Jason Fisher <jfisher@jfisher-15mbpr.ch.int>
2015-08-14 13:30:26 -0700

    udpated active record models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,26 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :followings
+  has_many :followees, :through => :followings, :foreign_key => :followee_id, :class_name => 'User'
+  has_many :followers, :through => :followings, :foreign_key => :follower_id, :class_name => 'User'
+
   validates :username,
             :presence => true,
             :uniqueness => {
                 :case_sensitive => false
-            } # etc.
+            }
+  validates :email,
+            :presence => true,
+            :uniqueness => {
+                :case_sensitive => false
+            }
+  validates_email_format_of :email, :message => 'does not look like a valid email address'
+
+  def follow(other_user_id)
+    new_following = Following.new(:follower_id => self.id, :followee_id => other_user_id)
+    logger.warn "created new following: #{new_following}"
+    new_following.save!
+  end
+
 end
