5f2b51e041262d85f9b3c9f8b5917b1f04187817
Jason Fisher <jfisher@castlighthealth.com>
2015-08-15 22:44:44 -0700

    added validation tests to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,33 +15,44 @@ RSpec.describe User, type: :model do
 
     it "fails validation with no username" do
      expect(User.create(:username => nil).errors[:username].size).to eq(1)
+     expect(User.create(:username => nil).errors[:username]).to include("can't be blank")
     end
 
-    it "fails validation with no email" do
-      expect(User.create(:email => nil).errors[:email].size).to eq(1)
+    it "validates uniqueness of usernames" do
+      @user_1.save!
+      expect(User.create(:username => @user_1.username).errors[:username].size).to eq(1)
+      expect(User.create(:username => @user_1.username).errors[:username]).to include("has already been taken")
     end
 
     it "fails validation with no password" do
       expect(User.create(:password => nil).errors[:password].size).to eq(1)
+      expect(User.create(:password => nil).errors[:password]).to include("can't be blank")
     end
 
-
-    it "should validate uniqueness of usernames" do
-      @user_1.save!
-
-    end
-
-    it "should validate uniqueness of emails" do
-
+    it "fails validation with no email" do
+      expect(User.create(:email => nil).errors[:email].size).to eq(2)
+      expect(User.create(:email => nil).errors[:email]).to include("can't be blank")
+      expect(User.create(:email => nil).errors[:email]).to include("does not look like a valid email address")
     end
 
     it "should validate format of email address" do
-
+      expect(User.create(:email => "invalidaddress@invalid").errors[:email].size).to eq(2)
+      expect(User.create(:email => "invalidaddress@invalid").errors[:email]).to include("is invalid") #would prefer only 1 error message but this is existing behavior
+      expect(User.create(:email => "invalidaddress@invalid").errors[:email]).to include("does not look like a valid email address")
     end
 
-    it "should validate presence of password"
+    it "should validate uniqueness of email address" do
+      @user_1.save!
+      expect(User.create(:email => @user_1.email).errors[:email].size).to eq(1)
+      expect(User.create(:email => @user_1.email).errors[:email]).to include("has already been taken")
+    end
 
-    it "should validate password and confirm password match"
+    it "should be case-insensitive when validating uniqueness of email address" do
+      @user_1.save!
+      expect(User.create(:email => @user_1.email.upcase).errors[:email].size).to eq(1)
+      expect(User.create(:email => @user_1.email.upcase).errors[:email]).to include("has already been taken")
+    end
+
   end
 
 end
