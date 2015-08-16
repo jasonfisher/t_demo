076b7b4a4689361233468ec63db2f14858a70e14
Jason Fisher <jfisher@castlighthealth.com>
2015-08-16 01:14:57 -0700

    added validations to following model

--- app/models/following.rb
+++ app/models/following.rb
@@ -4,6 +4,9 @@ class Following < ActiveRecord::Base
   belongs_to :followee, :class_name => 'User', :foreign_key => :follower_id
   belongs_to :follower, :class_name => 'User', :foreign_key => :followee_id
 
-  validates_uniqueness_of :follower_id, :scope => [:followee_id]
+  validates :follower_id, :presence => true
+  validates :followee_id, :presence => true
+
+  validates_uniqueness_of :follower_id, :scope => [:followee_id], :message => "already following that user"
 
 end
