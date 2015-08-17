cfd1858dd580210bcfb04756063b8248521fe7e8
Jason Fisher <jfisher@castlighthealth.com>
2015-08-17 10:52:10 -0700

    updated user followees method and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,21 @@ class User < ActiveRecord::Base
     Following.create(:follower_id => self.id, :followee_id => other_user.id)
   end
 
+  # NOTE: this should optimally also be cleaned up with detangling of User and Following models in the future
+
+  def followees
+
+#TODO! REFACTOR: this is an N+1 query situation that needs eager loading to fix it;
+# however, finding optimal way via with AR in rails 4 was slow and ambigiuous, so temp-only doing N+1 way to make it work (n is still small for now, at least, anyway)
+
+    followee_ids = Following.where(:follower_id => self.id).pluck(:followee_id)
+    followees = []
+    followee_ids.each do |followee_id|
+      followees << User.find(followee_id)
+    end
+    followees
+  end
+
   def follows?(user_id)
     following = Following.find(:follower_id => @self.id, :followee_id => user_id)
     return following ? true : false
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -65,13 +65,14 @@ RSpec.describe User, type: :model do
      end
   end
 
-  context "when following another user" do
+  context "when following" do
     before (:each) do
       @follower = FactoryGirl.create(:user)
       @followee = FactoryGirl.create(:user)
     end
 
     it "should create and return a new Following" do
+      #NOTE: refactor to better AR usage here
       expect(@follower.follow(@followee)).to eq(Following.where(["follower_id = ? and followee_id = ?", @follower.id, @followee.id]).first)
     end
 
@@ -82,7 +83,15 @@ RSpec.describe User, type: :model do
       expect(@follower.follow(@followee).errors[:follower_id]).to include("already following that user")
     end
 
-    # it "its followees should include the newly-followed User"
+    # see NOTES at bottom of page about ActiveModel::Relation vs [User] types
+    # NOTE: this is a test of the followees method -- refactor elsewhere? (what is best practice here?)
+    it "should add the new User to followees return value" do
+      expect(followees.size).to eq(1) #self only
+      @follower.follow(@followee)
+      followees = @follower.followees
+      expect(followees.size).to eq(2) #self and new followee
+      expect(followees).to eq(User.find([@follower.id, @followee.id]))
+    end
 
     # it "the followed User's followers should include the newly-following User"
     #
@@ -94,12 +103,20 @@ RSpec.describe User, type: :model do
 
   end
 
-  # context "when un-following another user" do
-  #   it "should not let a User un-follow themself"
-  #
-  #   it "should validate the Following already exists"
-  #
-  #   it "should return false from follows? method"
-  # end
+  context "when un-following" do
+    it "should not let a User un-follow themselves"
+
+    it "should validate the Following already exists"
+
+    it "should return false from follows? method"
+  end
 
 end
+
+#  NOTES ABOUT ACTIVERELATION VS AR OBJECT ISSUES
+#  FOR @user.followees continually getting ActiveRelation instead of array of user objects, e.g. rspec error message below
+#   unclear how to change either one to the other; even adding load explicitly into followee method of User object failed to produce actual User objects into the return value
+# expected: [#<User id: 564, email: "username4@castlighthealth.com", encrypted_password: "$2a$04$2k1IX/LS6NS0GG0ZZoovSu4IlfBpWWeYrkY8TqxPu3Z...", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, created_at: "2015-08-17 17:31:54", updated_at: "2015-08-17 17:31:54", username: "username4">, #<User id: 565, email: "username5@castlighthealth.com", encrypted_password: "$2a$04$HDitl/fPvPIV/txKx1mgvO/CEYy9i6V3DsPX95R8hEu...", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, created_at: "2015-08-17 17:31:54", updated_at: "2015-08-17 17:31:54", username: "username5">]
+#     got: [#<ActiveRecord::Relation [#<User id: 564, email: "username4@castlighthealth.com", encrypted_password: "$2a$04$2k1IX/LS6NS0GG0ZZoovSu4IlfBpWWeYrkY8TqxPu3Z...", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, created_at: "2015-08-17 17:31:54", updated_at: "2015-08-17 17:31:54", username: "username4">]>, #<ActiveRecord::Relation [#<User id: 565, email: "username5@castlighthealth.com", encrypted_password: "$2a$04$HDitl/fPvPIV/txKx1mgvO/CEYy9i6V3DsPX95R8hEu...", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, created_at: "2015-08-17 17:31:54", updated_at: "2015-08-17 17:31:54", username: "username5">]>]
+
+# SOLUTION: ultimately the fixit was changing a line in user.followees from .where(:id => X) to .find(x), which then returned a User object, not a relation -- I need to read up further on relations
