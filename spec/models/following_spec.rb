1b760b249f977447a0e5442828884fb57753e768
Jason Fisher <jfisher@castlighthealth.com>
2015-08-18 15:13:44 -0700

    cleaned up following spec

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -23,42 +23,6 @@ RSpec.describe Following, type: :model do
         expect(Following.create(:followee_id => user_1.id, :follower_id => user_2.id).errors[:follower_id]).to include("already following that user")
       end
 
-
-      # it "validates uniqueness of usernames" do
-      #   @user_1.save!
-      #   expect(User.create(:username => @user_1.username).errors[:username].size).to eq(1)
-      #   expect(User.create(:username => @user_1.username).errors[:username]).to include("has already been taken")
-      # end
-      #
-      # it "fails validation with no password" do
-      #   expect(User.create(:password => nil).errors[:password].size).to eq(1)
-      #   expect(User.create(:password => nil).errors[:password]).to include("can't be blank")
-      # end
-      #
-      # it "fails validation with no email" do
-      #   expect(User.create(:email => nil).errors[:email].size).to eq(2)
-      #   expect(User.create(:email => nil).errors[:email]).to include("can't be blank")
-      #   expect(User.create(:email => nil).errors[:email]).to include("does not look like a valid email address")
-      # end
-      #
-      # it "should validate format of email address" do
-      #   expect(User.create(:email => "invalidaddress@invalid").errors[:email].size).to eq(2)
-      #   expect(User.create(:email => "invalidaddress@invalid").errors[:email]).to include("is invalid") #would prefer only 1 error message but this is existing behavior
-      #   expect(User.create(:email => "invalidaddress@invalid").errors[:email]).to include("does not look like a valid email address")
-      # end
-      #
-      # it "should validate uniqueness of email address" do
-      #   @user_1.save!
-      #   expect(User.create(:email => @user_1.email).errors[:email].size).to eq(1)
-      #   expect(User.create(:email => @user_1.email).errors[:email]).to include("has already been taken")
-      # end
-      #
-      # it "should be case-insensitive when validating uniqueness of email address" do
-      #   @user_1.save!
-      #   expect(User.create(:email => @user_1.email.upcase).errors[:email].size).to eq(1)
-      #   expect(User.create(:email => @user_1.email.upcase).errors[:email]).to include("has already been taken")
-      # end
-
     end
 
   end
