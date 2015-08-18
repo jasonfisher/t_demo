0c0fabd258c91e573caec137babb00572f5225c2
Jason Fisher <jfisher@castlighthealth.com>
2015-08-18 15:14:12 -0700

    added to tweet model and spec

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,10 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
 
+  MAX_TWEET_LENGTH = 144
+
   validates :user_id, :presence => true
   validates :content, :presence => true
-  validates_length_of :content, :maximum => 144, :message => "tweets can not be more than 144 characters long"
+  validates_length_of :content, :maximum => MAX_TWEET_LENGTH, :message => "tweets can not be more than 144 characters long"
 
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Tweet, type: :model do
   #   @tweet = FactoryGirl.build(:tweet)
   # end
 
-  context "when creating a user" do
+  context "when creating a tweet" do
 
     it "should fail validation with no user_id" do
       expect(Tweet.create(:user_id => nil).errors[:user_id]).to eq(["can't be blank"])
@@ -22,6 +22,17 @@ RSpec.describe Tweet, type: :model do
       expect(Tweet.create(:content => long_string).errors[:content]).to eq(["tweets can not be more than 144 characters long"])
     end
 
+  end
 
+  it "should return the creating User" do
+    @tweet = FactoryGirl.build(:tweet)
+    expect(@tweet.user.class.name).to eq("User")
+  end
+
+  it "should return its content" do
+    @user = FactoryGirl.create(:user)
+    content_string = "here is some content!"
+    tweet = Tweet.create(:user_id => @user.id, :content => content_string)
+    expect(tweet.content).to eq(content_string)
   end
 end
