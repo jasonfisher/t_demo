6fe45452d5597396953ea4c3086d24248cb8906e
Jason Fisher <jfisher@castlighthealth.com>
2015-08-19 13:50:00 -0700

    updated tweet spec

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -8,31 +8,57 @@ RSpec.describe Tweet, type: :model do
 
   context "when creating a tweet" do
 
-    it "should fail validation with no user_id" do
-      expect(Tweet.create(:user_id => nil).errors[:user_id]).to eq(["can't be blank"])
+    context "with invalid parameters" do
+      it "should fail validation with no user_id" do
+        expect(Tweet.create(:user_id => nil).errors[:user_id]).to eq(["can't be blank"])
+      end
+
+      it "should fail validation with no content" do
+        expect(Tweet.create(:content => nil).errors[:content]).to eq(["can't be blank"])
+      end
+
+      it "should fail validation if content > Tweet::MAX_TWEET_LENGTH characters" do
+        long_string = "******************************************************************************************************************************************************"
+        expect(long_string.length).to eq(150)
+        expect(Tweet.create(:content => long_string).errors[:content]).to eq(["tweets can not be more than 144 characters long"])
+      end
     end
 
-    it "should fail validation with no content" do
-      expect(Tweet.create(:content => nil).errors[:content]).to eq(["can't be blank"])
-    end
+#TODO: this section is awkward and/or unnecessary
+    context "with valid parameters" do
+      it "should return the creating User" do
+        @tweet = create(:tweet)
+        expect(@tweet.user.class.name).to eq("User")
+      end
 
-    it "should fail validation if content > Tweet::MAX_TWEET_LENGTH characters" do
-      long_string = "******************************************************************************************************************************************************"
-      expect(long_string.length).to eq(150)
-      expect(Tweet.create(:content => long_string).errors[:content]).to eq(["tweets can not be more than 144 characters long"])
+      it "should return its content" do
+        @user = create(:user)
+        content_string = "here is some content!"
+        tweet = Tweet.create(:user_id => @user.id, :content => content_string)
+        expect(tweet.content).to eq(content_string)
+      end
+
+      it "should return its created_at time" do
+        tweet = create(:tweet)
+        expect(tweet.created_at.class.name).to eq("Time")
+      end
+
     end
 
   end
 
-  it "should return the creating User" do
-    @tweet = build(:tweet)
-    expect(@tweet.user.class.name).to eq("User")
+  context "when deleting a tweet" do
+    it "should remove the tweet from the database"
+
+    it "should remove the tweet from the list returned by get_tweets"
   end
 
-  it "should return its content" do
-    @user = create(:user)
-    content_string = "here is some content!"
-    tweet = Tweet.create(:user_id => @user.id, :content => content_string)
-    expect(tweet.content).to eq(content_string)
+  context "update is called on a tweet" do
+    it "should not allow the user_id to be changed"
+
+    it "should not allow the created_at time to be changed"
+
+    it "should not allow the content to be changed"
+
   end
 end
