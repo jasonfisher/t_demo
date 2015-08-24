c84de6fcb3a2152b56e3cec0d741f68940062a18
Jason Fisher <jfisher@castlighthealth.com>
2015-08-24 11:05:26 -0700

    added to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,33 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-  10.times do |i|
-    User.create(:username => "user_#{i}", :email => "user#{i}@example.com", :password => "password")
+  NUM_USERS = 10
+
+  #create some users
+  NUM_USERS.times do |i|
+    User.create(:username => "user#{i}", :email => "user#{i}@example.com", :password => "pppppppp")
   end
 
+  #create some tweets by those users
   User.all.each do |user|
-    10.times do
+    Random.rand(NUM_USERS).times do
       user.create_tweet(Faker::Lorem::sentence)
     end
   end
+
+  #have each user follow some random # of other users
+  all_users = User.all
+  all_users.each do |user|
+    rand_num_times = Random.rand(NUM_USERS)
+    STDERR.puts "user #{user.username} will try to follow #{rand_num_times} other users..."
+    rand_num_times.times do |i|
+      begin
+        user.follow(all_users[i-1])
+      rescue RuntimeError => re
+        #user tried to friend themselves
+        # STDERR.puts("oops, can't follow yourself...")
+      end
+    end
+  end
+
+  STDERR.puts("create: #{User.count} users, #{Following.count} followings, #{Tweet.count} tweets" )
