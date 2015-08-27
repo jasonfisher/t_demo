a071c45fb7e38f058b4a18cd128cc8d9bb3ac497
Jason Fisher <jfisher@castlighthealth.com>
2015-08-27 11:08:36 -0700

    updated comments in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,12 +36,8 @@ class UsersController < ApplicationController
     @unfollowed_users = @user.unfollowed_users - [@user]
   end
 
-#NOTE: there are some errors happening where users are showing as followed but then fail to get unfollowed and vice versa
-#      additional model specs did not reveal any underlying issues so doing resuces here to deal with these failures
   def follow_user
     @user_to_follow = User.find_by_id(params[:id])
-#TODO: handle error cases: user does not exist, user is already following, call to user.follow fails with error
-#TODO: do better redirect with next link as param somehow
     begin
       current_user.follow(@user_to_follow)
     rescue RuntimeError => re
@@ -53,8 +49,6 @@ class UsersController < ApplicationController
   def unfollow_user
     @user_to_unfollow = User.find_by_id(params[:id])
     logger.warn "current user #{current_user.id} trying to unfollow other user #{@user_to_unfollow.id}"
-#TODO: handle error cases: user does not exist, user is already following, call to user.follow fails with error
-#TODO: do better redirect with next link as param somehow
     begin
       current_user.unfollow(@user_to_unfollow)
     rescue RuntimeError => re
